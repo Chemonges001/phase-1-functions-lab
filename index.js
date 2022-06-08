9f08643445d5f5bb27749d570eeb44d493775712
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-08 11:31:47 +0300

    commit tree ffd331d85af2c1d47591e8490dff10cea76ee476

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blockNumber){
+  if (blockNumber > 42) {
+      return (blockNumber-42)
+  }
+  else{
+      return (42-blockNumber)
+  }
+}
+function distanceFromHqInFeet(blockNumber){
+    return distanceFromHqInBlocks(blockNumber)*264
+}
+function distanceTravelledInFeet(blockA, blockB){
+    if (blockA > blockB){
+        return ((blockA -blockB)*264)
+    }
+    else {
+        return((blockB-blockA)*264)
+    }
+}
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(destination, start);
+    if (distance < 400){
+        return 0;
+    }
+
+    else if (distance >= 400 && distance <=2000) {
+    return (distance - 400)*0.02;
+    }
+    else if (distance > 2000 && distance <= 2500) {
+        return 25
+    }
+    else if (distance > 2500){
+        return 'cannot travel that far';
+    }
+}
